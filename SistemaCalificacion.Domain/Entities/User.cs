b5122cee9d8b14658599917ff941de3410cad716
JoelGarcia <JoeleGarcia@live.com>
2025-09-21 23:05:33 -0400

    Entidad User de la capa de dominio

---
+++ SistemaCalificacion.Domain/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+        public required string Cedula { get; set; }
+        public required string Matricula { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public User()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+
+}
