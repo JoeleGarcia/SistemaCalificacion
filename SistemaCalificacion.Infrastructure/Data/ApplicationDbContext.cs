715bb29c6de863270de89db12d7fe96dfe5970af
JoelGarcia <JoeleGarcia@live.com>
2025-09-21 23:04:39 -0400

    agregando ApplicationDbContext

---
+++ SistemaCalificacion.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaCalificacion.Domain.Entities;
+
+
+namespace SistemaCalificacion.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext( DbContextOptions<DbContext> options) : base(options) { }
+
+
+    }
+}
