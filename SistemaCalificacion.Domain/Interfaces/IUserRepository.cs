c3f41e705338a23beaa1a14464f5d24bf0f3feed
JoelGarcia <JoeleGarcia@live.com>
2025-09-24 00:22:46 -0400

    Update: Interface de la entidad User desde la capa de Dominio

---
+++ SistemaCalificacion.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using SistemaCalificacion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+
+        Task<bool> AddUserAsync(User user);
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByUsernameAsync(string username);
+        Task<bool> IsEmailRegisteredAsync(string email);
+        Task<bool> IsUsernameRegisteredAsync(string username);
+        Task<bool> ValidateCredentialsAsync(string username, string password);
+
+    }
+}
