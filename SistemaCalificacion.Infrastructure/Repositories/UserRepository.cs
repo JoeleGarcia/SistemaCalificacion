d20955ff003d1fa2a700ab1338c7d7adbcb611e3
JoelGarcia <JoeleGarcia@live.com>
2025-09-24 00:24:56 -0400

    Updates: Implementacion de la interface IUserRepository en la capa de Infraestructura

---
+++ SistemaCalificacion.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaCalificacion.Domain.Entities;
+using SistemaCalificacion.Domain.Interfaces;
+using SistemaCalificacion.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly ApplicationDbContext _dbcontext;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _dbcontext = context;
+        }
+
+        public Task<bool> AddUserAsync(User user)
+        {
+
+            _dbcontext.Users.Add(user);
+            var result = _dbcontext.SaveChangesAsync();
+            return Task.FromResult(result.Result > 0);
+
+        }
+
+        public Task<User?> GetByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsEmailRegisteredAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUsernameRegisteredAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ValidateCredentialsAsync(string username, string password)
+        {
+            var user = await _dbcontext.Users.FirstOrDefaultAsync(u => u.Username == username);
+            if (user != null)
+            {
+                if (user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
