3078c61de131e8bcd07a77849253709ffd43f72f
JoelGarcia <JoeleGarcia@live.com>
2025-09-24 00:41:36 -0400

    Update: Se crea el controlador Account para el login y registro

---
+++ SistemaCalificacion.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCalificacion.Application.DTOs;
+using SistemaCalificacion.Application.Interfaces;
+using SistemaCalificacion.Application.Services;
+
+namespace SistemaCalificacion.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
