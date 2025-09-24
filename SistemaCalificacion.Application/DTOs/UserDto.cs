71c971f2b51e00df607cfb9f17852a98bc573be9
JoelGarcia <JoeleGarcia@live.com>
2025-09-24 00:20:55 -0400

    Updates: DTOs e interface para el Login y Registro

---
+++ SistemaCalificacion.Application/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SistemaCalificacion.Application.DTOs
+{
+    public record UserDto(int Id, string Username, string Password, string Email, string Cedula, string Matricula);
+
+    public record LoginUserDto(string Username, string Password);
+
+    public record RegisterUserDto(string Username, string Password , string Email , string Cedula , string Matricula);
+
+}
---
+++ SistemaCalificacion.Application/Interfaces/ILoginService.cs
@@ -0,0 +1,15 @@
+﻿using SistemaCalificacion.Application.DTOs;
+
+namespace SistemaCalificacion.Application.Interfaces
+{
+    public interface ILoginService
+    {
+
+        Task<bool> RegisterAsync(RegisterUserDto registerDto);
+        Task<bool> LoginAsync(string username , string password);
+        Task<bool> IsEmailRegisteredAsync(string email);
+        Task<bool> IsUsernameRegisteredAsync(string username);
+        Task<string> HashPassword(string password);
+
+    }
+}
