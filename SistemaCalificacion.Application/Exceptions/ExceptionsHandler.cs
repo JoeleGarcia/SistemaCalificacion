2683d2d4eca2ae43682fc81eb72dbecb1011d05d
JoelGarcia <JoeleGarcia@live.com>
2025-10-01 00:01:26 -0400

    Refactor: Se agrego un manejador de excepciones desde la capa de aplicaciones hacia la capa Presentation

---
+++ SistemaCalificacion.Application/Exceptions/ExceptionsHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entityName, object key)
+            : base($"La entidad \"{entityName}\" con clave ({key}) no fue encontrada.")
+        {
+        }
+    }
+
+    public class InfrastructureException : Exception
+    {
+        public InfrastructureException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
