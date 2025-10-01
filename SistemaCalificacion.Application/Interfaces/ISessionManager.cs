2f8a6a29c9c4d83d246cc12d7323d36262414f39
JoelGarcia <JoeleGarcia@live.com>
2025-10-01 00:02:45 -0400

    Feat: Interfaz para el manejo de sesion

---
+++ SistemaCalificacion.Application/Interfaces/ISessionManager.cs
@@ -0,0 +1,14 @@
+﻿using SistemaCalificacion.Application.DTOs;
+using SistemaCalificacion.Domain.Entities;
+
+
+namespace SistemaCalificacion.Application.Interfaces
+{
+    public interface ISessionManager
+    {
+
+        Task SignInAsync(User user);
+        Task SignOutAsync();
+
+    }
+}
