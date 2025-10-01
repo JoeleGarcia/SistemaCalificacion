ca5f9a11192dd3f373e119f3d07956ddb83487cc
JoelGarcia <JoeleGarcia@live.com>
2025-10-01 00:37:33 -0400

    Feat: Se implementa el uso del nuevo metodo de autenticacion y manejo de la sesion para los controllers

--- SistemaCalificacion.Application/Services/LoginService.cs
+++ SistemaCalificacion.Application/Services/LoginService.cs
@@ -1,4 +1,5 @@
 ﻿using SistemaCalificacion.Application.DTOs;
+using SistemaCalificacion.Application.Exceptions;
 using SistemaCalificacion.Application.Interfaces;
 using SistemaCalificacion.Domain.Entities;
 using SistemaCalificacion.Domain.Interfaces;
@@ -16,10 +17,13 @@ namespace SistemaCalificacion.Application.Services
     {
 
         private readonly IUserRepository _userRepository;
+        private readonly ISessionManager _sessionManagerRepository;
 
-        public LoginService(IUserRepository userRepository)
+
+        public LoginService(IUserRepository userRepository , ISessionManager sessionManagerRepository)
         {
             _userRepository = userRepository;
+            _sessionManagerRepository = sessionManagerRepository;
         }
 
         public Task<string> HashPassword(string password)
@@ -37,27 +41,49 @@ namespace SistemaCalificacion.Application.Services
             throw new NotImplementedException();
         }
 
-        public async Task<bool> LoginAsync(string username, string password)
+
+        public async Task<UserDto> LoginAsync(string username, string password)
         {
             var response = await _userRepository.ValidateCredentialsAsync(username, password);
-            return response;
+
+            if (response is null)
+            {
+                return null;
+            }
+
+            await _sessionManagerRepository.SignInAsync(response);
+
+            return new UserDto(response.Nombre, response.Apellido, response.Username, response.Email, response.Role, response.Status);
+            
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _sessionManagerRepository.SignOutAsync();
         }
 
         public async Task<bool> RegisterAsync(RegisterUserDto registerDto)
         {
-
-            var user = new User
+            try
             {
-                Username = registerDto.Username,
-                Password = registerDto.Password,
-                Matricula = registerDto.Matricula,
-                Cedula = registerDto.Cedula,
-                Email = registerDto.Email
-            };
+                var user = new User
+                {
+                    Username = registerDto.Username,
+                    Password = registerDto.Password,
+                    Email = registerDto.Email,
+                    Nombre = registerDto.Nombre,
+                    Apellido = registerDto.Apellido                    
+                };
 
-            var userRegistered = await _userRepository.AddUserAsync(user);
+                var userRegistered = await _userRepository.AddUserAsync(user);
 
-            return userRegistered;
+                return userRegistered;
+            }
+            catch (InfrastructureException ex)
+            {
+                throw new ApplicationException("No se pudo registrar el usuario. Intente más tarde.", ex);
+            }
+
         }
 
   
