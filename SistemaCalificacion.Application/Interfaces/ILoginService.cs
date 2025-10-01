7021039675db4917486cdd85f73ac3cddeada956
JoelGarcia <JoeleGarcia@live.com>
2025-10-01 00:35:45 -0400

    Updates: Se actualiza la interfaz con lo necesario a usar en el proyecto por ahora

--- SistemaCalificacion.Application/Interfaces/ILoginService.cs
+++ SistemaCalificacion.Application/Interfaces/ILoginService.cs
@@ -6,9 +6,8 @@ namespace SistemaCalificacion.Application.Interfaces
     {
 
         Task<bool> RegisterAsync(RegisterUserDto registerDto);
-        Task<bool> LoginAsync(string username , string password);
-        Task<bool> IsEmailRegisteredAsync(string email);
-        Task<bool> IsUsernameRegisteredAsync(string username);
+        Task<UserDto> LoginAsync(string email , string password);
+        Task LogoutAsync();
         Task<string> HashPassword(string password);
 
     }
