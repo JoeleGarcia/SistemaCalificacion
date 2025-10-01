01c1139bfa7fc02e0f3d27272e59ffc4fa4a32a0
JoelGarcia <JoeleGarcia@live.com>
2025-10-01 00:05:08 -0400

    Feat: Implementacion de la interfaz ISessionManageren en la capa de infraestrcutura

---
+++ SistemaCalificacion.Infrastructure/Repositories/SessionManagerRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using SistemaCalificacion.Application.DTOs;
+using SistemaCalificacion.Application.Interfaces;
+using SistemaCalificacion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace SistemaCalificacion.Infrastructure.Repositories
+{
+    public class SessionManagerRepository : ISessionManager
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+
+        public SessionManagerRepository( IHttpContextAccessor httpContextAccessor) { 
+           _httpContextAccessor = httpContextAccessor;
+        }
+
+      
+        public async Task SignInAsync(User user)
+        {
+            var claims = new List<Claim>{
+                new Claim(ClaimTypes.Name, user.Nombre),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("Username" , user.Username.ToString()),
+                new Claim("Estatus" , user.Status.ToString())
+
+            };
+
+            var identity = new ClaimsIdentity(claims , "MyCookieAuth");
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+            await _httpContextAccessor.HttpContext!.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+        }
+
+        public async Task SignOutAsync()
+        {
+            await _httpContextAccessor.HttpContext!.SignOutAsync("MyCookieAuth");
+        }
+    }
+}
