022ad712b813d80c542e6a2b91e72b0f48acc468
JoelGarcia <JoeleGarcia@live.com>
2025-10-02 17:03:17 -0400

    Feat: Se implementa la interfaz IPasswordGenerator

---
+++ SistemaCalificacion.Infrastructure/Repositories/PasswordGeneratorRepository.cs
@@ -0,0 +1,28 @@
+﻿using SistemaCalificacion.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Infrastructure.Repositories
+{
+    public class PasswordGeneratorRepository : IPasswordGenerator
+    {
+        private static Random rnd = new Random();
+
+        public string GenerarPassword(int length = 12)
+        {
+
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string contrasena = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                contrasena += caracteres[rnd.Next(caracteres.Length)];
+            }
+
+            return contrasena;
+        }
+    }
+}
