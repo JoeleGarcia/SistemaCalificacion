c322824b66c01ac8afaf84b92086cfc672355d6f
JoelGarcia <JoeleGarcia@live.com>
2025-10-02 00:18:04 -0400

    Feat: Interfaz del Servicio de Estudiante

---
+++ SistemaCalificacion.Application/Interfaces/IEstudianteService.cs
@@ -0,0 +1,21 @@
+﻿using SistemaCalificacion.Application.DTOs;
+using SistemaCalificacion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Application.Interfaces
+{
+    public interface IEstudianteService
+    {
+        Task<EstudianteDto> AddEstudianteAsync(EstudianteDto estudiante);
+        Task<bool> UpdateEstudianteAsync(Guid Id, EstudianteDto estudiante);
+        Task DeleteEstudianteByIdAsync(Guid id);
+        Task<IEnumerable<EstudianteDto>> GetAllEstudianteAsync();
+        Task<bool> IsEmailEstudianteRegisteredAsync(string email);
+        Task<bool> IsUsernameEstudianteRegisteredAsync(string username);
+
+    }
+}
