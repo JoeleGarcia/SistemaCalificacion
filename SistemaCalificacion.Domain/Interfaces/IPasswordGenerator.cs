7c4cf20573d98dc273a1559c5dca2b1daadf3aeb
JoelGarcia <JoeleGarcia@live.com>
2025-10-02 17:02:46 -0400

    Feat: Se agrega una interfaz IPasswordGenerator para un generador de password desde la capa de Dominio

---
+++ SistemaCalificacion.Domain/Interfaces/IPasswordGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Application.Interfaces
+{
+    public interface IPasswordGenerator
+    {
+        string GenerarPassword(int length = 12);
+
+    }
+}
