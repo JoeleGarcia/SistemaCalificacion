4504e5480377fa8ea2e3357461397b7f49728288
JoelGarcia <JoeleGarcia@live.com>
2025-10-02 00:17:21 -0400

    Update: DTOs Estudiante

--- SistemaCalificacion.Application/DTOs/EstudianteDto.cs
+++ SistemaCalificacion.Application/DTOs/EstudianteDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaCalificacion.Application.DTOs
 {
-    public record EstudianteDto(int Id, string Nombre, string Apellido, string Username, string EmailInstitucional, string EmailPersonal, string Password, int Matricula, int Cedula ,string Carrera ,string Role, bool Status);
+    public record EstudianteDto(Guid Id, string Nombre, string Apellido, string Username, string EmailInstitucional, string EmailPersonal, string Password, int Matricula, int Cedula ,string Carrera ,string? Role, bool Status);
 
 
 
