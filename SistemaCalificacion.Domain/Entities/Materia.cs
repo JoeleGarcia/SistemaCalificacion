bb41c8ca67c9eaba986659fdfd76abc4b12962ef
JoelGarcia <JoeleGarcia@live.com>
2025-10-09 00:51:11 -0400

    Update: Actualizacion para la entidad Materia

--- SistemaCalificacion.Domain/Entities/Materia.cs
+++ SistemaCalificacion.Domain/Entities/Materia.cs
@@ -1,13 +1,16 @@
 ﻿
+using System.Data;
+using System.Net.NetworkInformation;
+
 namespace SistemaCalificacion.Domain.Entities
 {
     public class Materia
     {
         public int Id { get; set; }
-        public required string Nombre { get; set; }
+        public string Nombre { get; set; }
         public string Descripcion { get; set; } = string.Empty;
         public bool Status { get; set; }
-        public ICollection<Calificaciones> Calificaciones { get; set; } = new HashSet<Calificaciones>();
+        public ICollection<Calificaciones> Calificaciones { get; set; }
 
     }
 }
