23669fcd376d3cd506c7eba1942c8d97afd1ddeb
JoelGarcia <JoeleGarcia@live.com>
2025-10-09 22:31:57 -0400

    Fix: Validaciones para las entidades Estudiantes y Calificaciones

---
+++ SistemaCalificacion.Application/Validator/CalificacionesDtoValidator.cs
@@ -0,0 +1,153 @@
+﻿using FluentValidation;
+using SistemaCalificacion.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Application.Validator
+{
+
+
+    //public class CalificacionesDtoValidator : AbstractValidator<CalificacionesDto>
+    //{
+    //    public CalificacionesDtoValidator()
+    //    {
+
+    //        RuleFor(x => x.EstudianteId)
+    //            .NotEmpty().WithMessage("El EstudianteId es obligatorio.");
+
+    //        RuleFor(x => x.MateriaId)
+    //            .GreaterThan(0).WithMessage("Debe seleccionar una materia válida.");
+
+    //        RuleFor(x => x.Calificacion1)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion1.HasValue)
+    //            .WithMessage("La Calificación 1 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Calificacion2)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion2.HasValue)
+    //            .WithMessage("La Calificación 2 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Calificacion3)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion3.HasValue)
+    //            .WithMessage("La Calificación 3 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Calificacion4)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion4.HasValue)
+    //            .WithMessage("La Calificación 4 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Examen)
+    //            .InclusiveBetween(0, 100).When(x => x.Examen.HasValue)
+    //            .WithMessage("El Examen debe estar entre 0 y 100.");
+    //    }
+
+    //}
+
+    public class CreateCalificacionesDtoValidator : AbstractValidator<CreateCalificacionesDto>
+    {
+        public CreateCalificacionesDtoValidator()
+        {
+
+            RuleFor(x => x.EstudianteId)
+                .NotEmpty().WithMessage("El EstudianteId es obligatorio.");
+
+            RuleFor(x => x.MateriaId)
+                .GreaterThan(0).WithMessage("Debe seleccionar una materia válida.");
+
+            RuleFor(x => x.Calificacion1)
+                .InclusiveBetween(0, 100).When(x => x.Calificacion1.HasValue)
+                .WithMessage("La Calificación 1 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Calificacion2)
+                .InclusiveBetween(0, 100).When(x => x.Calificacion2.HasValue)
+                .WithMessage("La Calificación 2 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Calificacion3)
+                .InclusiveBetween(0, 100).When(x => x.Calificacion3.HasValue)
+                .WithMessage("La Calificación 3 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Calificacion4)
+                .InclusiveBetween(0, 100).When(x => x.Calificacion4.HasValue)
+                .WithMessage("La Calificación 4 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Examen)
+                .InclusiveBetween(0, 100).When(x => x.Examen.HasValue)
+                .WithMessage("El Examen debe estar entre 0 y 100.");
+
+        }
+    }
+
+
+
+    public class UpdateCalificacionesDtoValidator : AbstractValidator<UpdateCalificacionesDto>
+    {
+        public UpdateCalificacionesDtoValidator()
+        {
+
+            RuleFor(x => x.EstudianteId)
+                .NotEmpty().WithMessage("El EstudianteId es obligatorio.");
+
+            RuleFor(x => x.MateriaId)
+                .GreaterThan(0).WithMessage("Debe seleccionar una materia válida.");
+
+            RuleFor(x => x.Calificacion1)
+                .InclusiveBetween(0, 100)
+                .WithMessage("La Calificación 1 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Calificacion2)
+                .InclusiveBetween(0, 100)
+                .WithMessage("La Calificación 2 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Calificacion3)
+                .InclusiveBetween(0, 100)
+                .WithMessage("La Calificación 3 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Calificacion4)
+                .InclusiveBetween(0, 100)
+                .WithMessage("La Calificación 4 debe estar entre 0 y 100.");
+
+            RuleFor(x => x.Examen)
+                .InclusiveBetween(0, 100)
+                .WithMessage("El Examen debe estar entre 0 y 100.");
+
+        }
+    }
+
+
+
+    //public class SelectCalificacionesDtoValidator : AbstractValidator<SelectCalificacionesDto>
+    //{
+    //    public SelectCalificacionesDtoValidator()
+    //    {
+
+    //        RuleFor(x => x.EstudianteId)
+    //            .NotEmpty().WithMessage("El EstudianteId es obligatorio.");
+
+    //        RuleFor(x => x.MateriaId)
+    //            .GreaterThan(0).WithMessage("Debe seleccionar una materia válida.");
+
+    //        RuleFor(x => x.Calificacion1)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion1.HasValue)
+    //            .WithMessage("La Calificación 1 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Calificacion2)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion2.HasValue)
+    //            .WithMessage("La Calificación 2 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Calificacion3)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion3.HasValue)
+    //            .WithMessage("La Calificación 3 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Calificacion4)
+    //            .InclusiveBetween(0, 100).When(x => x.Calificacion4.HasValue)
+    //            .WithMessage("La Calificación 4 debe estar entre 0 y 100.");
+
+    //        RuleFor(x => x.Examen)
+    //            .InclusiveBetween(0, 100).When(x => x.Examen.HasValue)
+    //            .WithMessage("El Examen debe estar entre 0 y 100.");
+
+    //    }
+    //}
+
+}
---
+++ SistemaCalificacion.Application/Validator/EstudianteDtoValidator.cs
@@ -0,0 +1,105 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using SistemaCalificacion.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalificacion.Application.Validator
+{
+    public class CreateEstudianteDtoValidator : AbstractValidator<CreateEstudianteDto>
+    {
+        public CreateEstudianteDtoValidator()
+        {
+            RuleFor(x => x.Nombre)
+                .NotEmpty().WithMessage("El nombre es obligatorio.")
+                .MaximumLength(100).WithMessage("El nombre no puede exceder los 100 caracteres.");
+
+            RuleFor(x => x.Apellido)
+                .NotEmpty().WithMessage("El apellido es obligatorio.")
+                .MaximumLength(100).WithMessage("El apellido no puede exceder los 100 caracteres.");
+
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("El nombre de usuario es obligatorio.")
+                .MinimumLength(4).WithMessage("El nombre de usuario debe tener al menos 4 caracteres.");
+
+            RuleFor(x => x.EmailInstitucional)
+                .NotEmpty().WithMessage("El correo institucional es obligatorio.")
+                .EmailAddress(EmailValidationMode.AspNetCoreCompatible).WithMessage("Debe ingresar un correo institucional válido.");
+
+            RuleFor(x => x.Matricula)
+                .NotEmpty().WithMessage("La matrícula es obligatoria.")
+                .Must(SoloNumeros).WithMessage("La cédula solo debe contener caracteres numéricos.")
+                .Length(9).WithMessage("La matrícula no puede exceder los 9 caracteres.");
+
+            RuleFor(x => x.Cedula)
+                .NotEmpty().WithMessage("La cédula es obligatoria.")
+                .Must(SoloNumeros).WithMessage("La cédula solo debe contener caracteres numéricos.")
+                .Length(11).WithMessage("La cédula debe tener exactamente 11 caracteres.");
+
+            RuleFor(x => x.Carrera)
+                .NotEmpty().WithMessage("La carrera es obligatoria.");
+
+        }
+
+        private bool SoloNumeros(string value)
+        {
+            if (value != null && value.All(char.IsDigit))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+
+    public class UpdateEstudianteDtoValidator : AbstractValidator<UpdateEstudianteDto>
+    {
+        public UpdateEstudianteDtoValidator()
+        {
+            RuleFor(x => x.Nombre)
+                .NotEmpty().WithMessage("El nombre es obligatorio.")
+                .MaximumLength(100).WithMessage("El nombre no puede exceder los 100 caracteres.");
+
+            RuleFor(x => x.Apellido)
+                .NotEmpty().WithMessage("El apellido es obligatorio.")
+                .MaximumLength(100).WithMessage("El apellido no puede exceder los 100 caracteres.");
+
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("El nombre de usuario es obligatorio.")
+                .MinimumLength(4).WithMessage("El nombre de usuario debe tener al menos 4 caracteres.");
+
+            RuleFor(x => x.EmailInstitucional)
+                .NotEmpty().WithMessage("El correo institucional es obligatorio.")
+                .EmailAddress(EmailValidationMode.AspNetCoreCompatible).WithMessage("Debe ingresar un correo institucional válido.");
+
+            RuleFor(x => x.Matricula)
+                .NotEmpty().WithMessage("La matrícula es obligatoria.")
+                .Matches(@"^\d+$").WithMessage("La matrícula solo debe contener números.")
+                .Length(9).WithMessage("La matrícula no puede exceder los 11 caracteres.");
+
+            RuleFor(x => x.Cedula)
+                .NotEmpty().WithMessage("La cédula es obligatoria.")
+                .Must(SoloNumeros).WithMessage("La cédula solo debe contener caracteres numéricos.")
+                .Length(11).WithMessage("La cédula debe tener exactamente 11 caracteres.");
+
+            RuleFor(x => x.Carrera)
+                .NotEmpty().WithMessage("La carrera es obligatoria.");
+
+        }
+
+        private bool SoloNumeros(string value)
+        {
+            if (value != null && value.All(char.IsDigit))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
